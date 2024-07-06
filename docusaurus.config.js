787e75548bd3cc899326d3b0bcd08789d4ec32e5
ashjean <ashjean.dev@gmail.com>
2024-07-07 02:02:47 +0900

    - 테스트용 커밋

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Our Site',
-  tagline: 'We Know you hate Data..',
+  tagline: 'We Know you hate Data',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
